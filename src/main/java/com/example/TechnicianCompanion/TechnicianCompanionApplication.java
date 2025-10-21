db1578a3420a7fe2ee5c2976aec77996d82a2844
Flasney Medeiros <flasney.medeiros@outlook.com>
2025-10-20 21:23:11 -0300

    Initializing Project

---
+++ src/main/java/com/example/TechnicianCompanion/TechnicianCompanionApplication.java
@@ -0,0 +1,13 @@
+package com.example.TechnicianCompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicianCompanionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicianCompanionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TechnicianCompanion/TechnicianCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TechnicianCompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicianCompanionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
